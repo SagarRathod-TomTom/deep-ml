86a321e67b207402dafb1fc035a07ddb11089207
Sagar Rathod <sagar100rathod@gmail.com>
2020-01-14 21:47:22 +0530

    changes for computing running average loss

--- train.py
+++ train.py
@@ -42,21 +42,23 @@ class Trainer:
             device = torch.device("cpu")
 
         self.model.eval()
-        losses = []
+        running_loss = 0
         with torch.no_grad():
-            for X, y in tqdm(loader, total=len(loader), desc="{:12s}".format('Validation')):
+            bar = tqdm(total=len(loader), desc="{:12s}".format('Validation'))
+            for batch_index, (X, y) in enumerate(loader):
                 if use_gpu:
                     X = X.to(device)
                     y = y.to(device)
                 output = self.model(X)
                 loss = criterion(output, y)
-                losses.append(loss.item())
+                bar.update(1)
+                running_loss = running_loss + ((loss.item() - running_loss) / (batch_index + 1))
+                bar.set_postfix({'Val Loss': '{:.6f}'.format(running_loss)})
 
-        loss = np.mean(losses)
-        return loss
+        return running_loss
 
     def fit(self, criterion, train_loader, val_loader=None, epochs=10, use_gpu=False,
-              steps_per_epoch=500, save_model_afer_every_epoch=5, lr_scheduler=None):
+              steps_per_epoch=None, save_model_afer_every_epoch=5, lr_scheduler=None):
 
         """
         steps_per_epoch = should be around len(train_loader) / batch_size,
@@ -68,16 +70,20 @@ class Trainer:
         else:
             device = torch.device("cpu")
 
+        if steps_per_epoch is None:
+            steps_per_epoch = len(train_loader) + 1  # Number of batches + 1, since batch index starts with 0
+
         for epoch in range(self.epochs_completed, self.epochs_completed + epochs):
-            train_losses = []
+
             # Training mode
             self.model.train()
 
             # Iterate over batches
             step = 0
             lr_step_done = False
-            for batch_index, (X, y) in tqdm(enumerate(train_loader), total=steps_per_epoch,
-                                            desc="{:12s}".format('Training')):
+            running_train_loss = 0
+            bar = tqdm(total=steps_per_epoch, desc="{:12s}".format('Training'))
+            for batch_index, (X, y) in enumerate(train_loader):
 
                 if use_gpu:
                     X = X.to(device)
@@ -95,18 +101,16 @@ class Trainer:
                     lr_scheduler.step()
                     lr_step_done = True
 
-                train_losses.append(loss.item())
                 step = step + 1
+                bar.update(1)
+                running_train_loss = running_train_loss + ((loss.item() - running_train_loss) / step)
+                bar.set_postfix({'Train loss': '{:.6f}'.format(running_train_loss)})
                 if step % steps_per_epoch == 0:
                     break
-
-            train_loss = np.mean(train_losses)
-            stats_info = 'Epoch: {}/{}\tTrain Loss: {:.6f}'.format(epoch + 1, epochs, train_loss)
 
             val_loss = np.inf
             if val_loader is not None:
                 val_loss = self.validate(criterion, val_loader, use_gpu)
-                stats_info = stats_info + "\tVal Loss: {:.6f}".format(val_loss)
 
                 if isinstance(lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
                     lr_scheduler.step(val_loss)
@@ -116,20 +120,19 @@ class Trainer:
                 if val_loss < self.best_val_loss:
                     self.best_val_loss = val_loss
                     print('Saving best validation model.')
-                    self.save_model_optim_state('best_val_model.pt', epoch, train_loss=train_loss, val_loss=val_loss)
+                    self.save_model_optim_state('best_val_model.pt', epoch, train_loss=running_train_loss,
+                                                val_loss=val_loss)
 
             if lr_scheduler is not None and not lr_step_done:
                 lr_scheduler.step()
                 lr_step_done = True
 
-            # print training/validation statistics
-            print(stats_info)
             if epoch % save_model_afer_every_epoch == 0:
                 model_file_name = "model_epoch_{}.pt".format(epoch)
-                self.save_model_optim_state(model_file_name, epoch, train_loss=train_loss, val_loss=val_loss)
+                self.save_model_optim_state(model_file_name, epoch, train_loss=running_train_loss, val_loss=val_loss)
 
         # Save latest model at the end
-        self.save_model_optim_state("latest_model.pt", epoch, train_loss=train_loss, val_loss=val_loss)
+        self.save_model_optim_state("latest_model.pt", epoch, train_loss=running_train_loss, val_loss=val_loss)
 
     def predict_proba(self, loader, use_gpu=False):
 
