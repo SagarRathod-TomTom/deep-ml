af3a52d580175e4eda0d21cd96b660f445507d39
Sagar Rathod <sagar100rathod@gmail.com>
2020-06-06 20:26:28 +0530

    Implements show image batch functionality from loader or tensor image batch

--- deepml/utils.py
+++ deepml/utils.py
@@ -1,6 +1,9 @@
 import os
 import glob
+
 import torch
+from matplotlib import pyplot as plt
+import numpy as np
 from deepml.constants import RUN_DIR_NAME
 
 
@@ -26,3 +29,34 @@ def binarize(output: torch.FloatTensor, threshold: float = 0.50):
     return output.to(torch.uint8)
 
 
+def show_batch(loader, cols=4, figsize=(5, 5)):
+    samples = np.random.randint(0, len(loader.dataset), loader.batch_size)
+    plt.figure(figsize=figsize)
+    rows = int(np.ceil(loader.batch_size / cols))
+    for index, sample_index in enumerate(samples):
+        X, y = loader.dataset[sample_index]
+        X = X.numpy().transpose(1, 2, 0) # CWH -> WHC
+        plt.subplot(rows, cols, index + 1)
+        plt.imshow(X)
+        if y is not None:
+            target = y.item()
+            title = round(target, 2) if type(target) == float else target
+            plt.title(title)
+        plt.xticks([])
+        plt.yticks([])
+
+
+def plot_image_batch(img_batch: torch.Tensor, targets=None, cols=4, figsize=(5, 5)):
+    img_batch = img_batch.numpy().transpose((0, 2, 3, 1)) # BCWH --> BWHC
+    plt.figure(figsize=figsize)
+    samples = len(img_batch)
+    rows = int(np.ceil(samples / cols))
+    for index in range(samples):
+        plt.subplot(rows, cols, index + 1)
+        plt.imshow(img_batch[index])
+        if targets is not None:
+            target = targets[index].item()
+            title = round(target, 2) if type(target) == float else target
+            plt.title(title)
+        plt.xticks([])
+        plt.yticks([])
