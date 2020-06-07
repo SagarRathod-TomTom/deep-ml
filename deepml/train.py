24a8a93e3c186161f0bcdbbdcb3fceac02c26064
Sagar Rathod <sagar100rathod@gmail.com>
2020-06-07 23:00:47 +0530

    Refactors code based loading and saving model state and device usage

--- deepml/train.py
+++ deepml/train.py
@@ -7,50 +7,67 @@ from tqdm.auto import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
 from deepml import utils
-from deepml.constants import RUN_DIR_NAME
 from deepml.transforms import ImageNetInverseTransform
 
 
-class Trainer:
+class Learner:
 
-    def __init__(self, model, optimizer, model_save_path, load_saved_model=False,
-                 model_file_name='latest_model.pt'):
-        self.model = model
-        self.optimizer = optimizer
+    def __init__(self, model, optimizer, model_save_path, model_file_name='latest_model.pt',
+                 load_saved_model=False, load_optimizer_state=False, use_gpu=False):
+        self.__model = model
+        self.__optimizer = optimizer
         self.model_save_path = model_save_path
         self.epochs_completed = 0
         self.best_val_loss = np.inf
         self.lr_rates = {}
-        self.writer = SummaryWriter(os.path.join(model_save_path, RUN_DIR_NAME +
-                                                 str(utils.find_current_run_number(model_save_path))))
+        self.writer = SummaryWriter(model_save_path)
+        self.device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
+        self.__model = self.__model.to(self.device)
 
-        if load_saved_model:
-            self.load_saved_model(model_file_name)
+        if load_saved_model and model_file_name is not None:
+            self.load_saved_model(os.path.join(self.model_save_path, model_file_name),
+                                  load_optimizer_state)
 
-    def load_saved_model(self, model_file_name):
-        print('Loading Saved Model Weights.')
-        state_dict = torch.load(os.path.join(self.model_save_path, model_file_name))
-        self.model.load_state_dict(state_dict['model'])
-        self.optimizer.load_state_dict(state_dict['optimizer'])
-        self.epochs_completed = state_dict['epoch']
-        self.best_val_loss = state_dict['metrics']['val_loss']
+    def load_saved_model(self, model_path, load_optimizer_state=False):
+        if os.path.exists(model_path):
+            print('Loading Saved Model Weights.')
+            state_dict = torch.load(model_path)
+            self.__model.load_state_dict(state_dict['model'])
 
-    def save_model_optim_state(self, model_file_name, epoch, train_loss, val_loss):
-        torch.save({'model': self.model.state_dict(),
-                    'optimizer': self.optimizer.state_dict(),
-                    'metrics': {'train_loss': train_loss, 'val_loss': val_loss},
-                    'epoch': epoch
-                    },
-                    os.path.join(self.model_save_path, model_file_name))
+            if load_optimizer_state and 'optimizer' in state_dict and state_dict['optimizer'] == \
+                    self.__optimizer.__class__.__name__:
+                self.__optimizer.load_state_dict(state_dict['optimizer_state'])
 
-    def validate(self, criterion, loader, use_gpu=False, show_progress=True):
+            if 'epoch' in state_dict:
+                self.epochs_completed = state_dict['epoch']
+
+            if 'metrics' in state_dict and 'val_loss' in state_dict['metrics']:
+                self.best_val_loss = state_dict['metrics']['val_loss']
+        else:
+            print(f'{model_path} does not exist.')
+
+    def save(self, model_file_name, save_optimizer_state=False, epoch=None, train_loss=None, val_loss=None):
+        # Convert model into cpu before saving the model state
+        cpu_model = self.__model.to("cpu")
+        save_dict = {'model': cpu_model.state_dict()}
+
+        if save_optimizer_state:
+            save_dict['optimizer'] = self.__optimizer.__class__.__name__
+            save_dict['optimizer_state'] = self.__optimizer.state_dict()
+
+        if type(epoch) == int:
+            save_dict['epoch'] = epoch
+
+        if type(train_loss) == float and type(val_loss) == float:
+            save_dict['metrics'] = {'train_loss': train_loss, 'val_loss': val_loss}
+
+        torch.save(save_dict, os.path.join(self.model_save_path, model_file_name))
+
+    def validate(self, criterion, loader, show_progress=True):
         if loader is None:
             raise Exception('Loader cannot be None.')
 
-        device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
-        self.model = self.model.to(device)
-
-        self.model.eval()
+        self.__model.eval()
         running_loss = 0
 
         if show_progress:
@@ -58,10 +75,9 @@ class Trainer:
 
         with torch.no_grad():
             for batch_index, (X, y) in enumerate(loader):
-                if use_gpu:
-                    X = X.to(device)
-                    y = y.to(device)
-                output = self.model(X)
+                X = X.to(self.device)
+                y = y.to(self.device)
+                output = self.__model(X)
                 loss = criterion(output, y)
                 running_loss = running_loss + ((loss.item() - running_loss) / (batch_index + 1))
 
@@ -71,34 +87,33 @@ class Trainer:
 
         return running_loss
 
-    def fit(self, criterion, train_loader, val_loader=None, epochs=10, use_gpu=False,
-              steps_per_epoch=None, save_model_afer_every_epoch=5, lr_scheduler=None,
+    def fit(self, criterion, train_loader, val_loader=None, epochs=10, steps_per_epoch=None,
+            save_model_afer_every_epoch=5, lr_scheduler=None,
             viz_reverse_transform=None, show_progress=True):
 
         """
         steps_per_epoch = should be around len(train_loader),
             so that every example in the dataset gets covered in each epoch.
         """
-        device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
-        self.model = self.model.to(device)
 
         if steps_per_epoch is None:
             steps_per_epoch = len(train_loader)
 
         # Write graph to tensorboard
         if torch.cuda.is_available():
-            self.writer.add_graph(self.model, next(iter(train_loader))[0].cuda())
+            self.writer.add_graph(self.__model, next(iter(train_loader))[0].cuda())
 
         if viz_reverse_transform is None:
             viz_reverse_transform = ImageNetInverseTransform()
 
+        criterion = criterion.to(self.device)
         epochs = self.epochs_completed + epochs
         for epoch in range(self.epochs_completed, epochs):
 
             print('Epoch {}/{}:'.format(epoch + 1, epochs))
 
             # Training mode
-            self.model.train()
+            self.__model.train()
 
             # Iterate over batches
             step = 0
@@ -111,18 +126,16 @@ class Trainer:
                 print('Training...')
 
             for batch_index, (X, y) in enumerate(train_loader):
-
-                if use_gpu:
-                    X = X.to(device)
-                    y = y.to(device)
+                X = X.to(self.device)
+                y = y.to(self.device)
 
                 # zero the parameter gradients
-                self.optimizer.zero_grad()
+                self.__optimizer.zero_grad()
 
-                outputs = self.model(X)
+                outputs = self.__model(X)
                 loss = criterion(outputs, y)
                 loss.backward()
-                self.optimizer.step()
+                self.__optimizer.step()
 
                 if isinstance(lr_scheduler, torch.optim.lr_scheduler.CyclicLR):
                     lr_scheduler.step()
@@ -135,10 +148,11 @@ class Trainer:
                     bar.update(1)
                     bar.set_postfix({'Train loss': '{:.6f}'.format(running_train_loss)})
 
-                if step % steps_per_epoch == 0 and y.ndim > 1:
+                if step % steps_per_epoch == 0:
                     self.writer.add_images('Images/Train/Input/', viz_reverse_transform(X), epoch + 1)
-                    self.writer.add_images('Images/Train/Target', y, epoch + 1)
-                    self.writer.add_images('Images/Train/Output', utils.binarize(outputs), epoch + 1)
+                    if y.ndim > 1:
+                        self.writer.add_images('Images/Train/Target', y, epoch + 1)
+                        self.writer.add_images('Images/Train/Output', utils.binarize(outputs), epoch + 1)
                     break
 
             stats_info = 'Epoch: {}/{}\tTrain Loss: {:.6f}'.format(epoch + 1, epochs, running_train_loss)
@@ -152,11 +166,11 @@ class Trainer:
 
                 # Log lass batch of val images to viz
                 if viz_reverse_transform is not None and torch.cuda.is_available():
-                    self.model.eval()
+                    self.__model.eval()
                     with torch.no_grad():
                         X, y = next(iter(val_loader))
                         X, y = X.cuda(), y.cuda()
-                        outputs = self.model(X)
+                        outputs = self.__model(X)
                         if y.ndim > 1:
                             self.writer.add_images('Images/Val/Input/', viz_reverse_transform(X), epoch + 1)
                             self.writer.add_images('Images/Val/Target', y)
@@ -170,8 +184,11 @@ class Trainer:
                 if val_loss < self.best_val_loss:
                     self.best_val_loss = val_loss
                     print('Saving best validation model.')
-                    self.save_model_optim_state('best_val_model.pt', epoch, train_loss=running_train_loss,
-                                                val_loss=val_loss)
+                    self.save('best_val_model.pt',
+                              save_optimizer_state=True,
+                              epoch=epoch,
+                              train_loss=running_train_loss,
+                              val_loss=val_loss)
 
             if lr_scheduler is not None and not lr_step_done:
                 lr_scheduler.step()
@@ -182,36 +199,32 @@ class Trainer:
             self.writer.flush()
             if epoch % save_model_afer_every_epoch == 0:
                 model_file_name = "model_epoch_{}.pt".format(epoch)
-                self.save_model_optim_state(model_file_name, epoch, train_loss=running_train_loss, val_loss=val_loss)
+                self.save(model_file_name, save_optimizer_state=True, epoch=epoch,
+                          train_loss=running_train_loss, val_loss=val_loss)
 
         self.writer.close()
         # Save latest model at the end
-        self.save_model_optim_state("latest_model.pt", epoch, train_loss=running_train_loss,
-                                    val_loss=self.best_val_loss)
+        self.save("latest_model.pt", save_optimizer_state=True, epoch=epoch, train_loss=running_train_loss,
+                  val_loss=self.best_val_loss)
 
-    def predict_proba(self, loader, use_gpu=False):
-
-        device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
-        self.model = self.model.to(device)
-
-        self.model.eval()
-        preds = []
-        y_true = []
+    def predict(self, loader):
+        self.__model.eval()
+        predictions = []
+        targets = []
         with torch.no_grad():
             for X, y in tqdm(loader, total=len(loader), desc="{:12s}".format('Prediction')):
-                if use_gpu:
-                    X = X.to(device)
-                pred = self.model(X).cpu()
-                preds.append(pred)
-                y_true.append(y)
+                X = X.to(self.device)
+                pred = self.__model(X).cpu()
+                predictions.append(pred)
+                targets.append(y)
 
-        return torch.cat(preds), torch.cat(y_true)
+        predictions = torch.cat(predictions)
+        targets = torch.cat(targets) if type(targets[0]) == torch.Tensor else np.hstack(targets).tolist()
+
+        return predictions, targets
 
     def extract_features(self, loader, no_of_features, features_csv_file, iterations=1,
-                         target_known=True, use_gpu=False):
-
-        device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
-        self.model = self.model.to(device)
+                         target_known=True):
 
         fp = open(features_csv_file, 'w')
         csv_writer = csv.writer(fp)
@@ -225,13 +238,13 @@ class Trainer:
         csv_writer.writerow(cols)
         fp.flush()
 
-        self.model.eval()
+        self.__model.eval()
         with torch.no_grad():
             for iteration in range(iterations):
                 print('Iteration:', iteration + 1)
                 for X, y in tqdm(loader, total=len(loader), desc='Feature Extraction'):
-                    X = X.to(device)
-                    feature_set = self.model(X).cpu().numpy()
+                    X = X.to(self.device)
+                    feature_set = self.__model(X).cpu().numpy()
 
                     if target_known:
                         y = y.numpy().reshape(-1,1)
