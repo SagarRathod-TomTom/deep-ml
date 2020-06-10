4795edd2cd82658562082624865df0959126f959
Sagar Rathod <sagar100rathod@gmail.com>
2020-06-10 09:48:42 +0530

    Refactors code

--- deepml/predict.py
+++ deepml/predict.py
@@ -8,29 +8,31 @@ import torch
 
 class Predictor(ABC):
 
-    def __init__(self, model: torch.nn.Module, model_save_path: str, model_file_name='best_val_model.pt'):
+    def __init__(self, model: torch.nn.Module, model_save_path=None, model_file_name='best_val_model.pt'):
         super(Predictor, self).__init__()
-        state_dict = torch.load(os.path.join(model_save_path, model_file_name))
-        self.model = model
-        self.model.load_state_dict(state_dict['model'])
+        self.__model = model
+
+        if model_save_path and os.path.exists(os.path.join(model_save_path, model_file_name)):
+            state_dict = torch.load(os.path.join(model_save_path, model_file_name))
+            self.__model.load_state_dict(state_dict['model'])
 
     @abstractmethod
     def predict(self, loader, use_gpu=False):
         pass
 
     @abstractmethod
-    def predict_one(self, input: torch.Tensor, use_gpu=False):
+    def predict_one(self,  input: torch.Tensor, use_gpu=False):
         pass
 
 
 class SemanticSegmentationPredictor(Predictor):
 
-    def __init__(self, model: torch.nn.Module, model_save_path: str,
-                 model_file_name: str):
+    def __init__(self, model: torch.nn.Module, model_save_path=None,
+                 model_file_name=None):
         super(SemanticSegmentationPredictor, self).__init__(model, model_save_path,
                                                             model_file_name)
 
-    def predict_one(self, input: torch.Tensor, use_gpu=False):
+    def predict_one(self,  input: torch.Tensor, use_gpu=False):
         raise NotImplementedError
 
     def predict(self, loader, use_gpu=False):
@@ -39,8 +41,8 @@ class SemanticSegmentationPredictor(Predictor):
 
 class ImageRegressionPredictor(Predictor):
 
-    def __init__(self, model: torch.nn.Module, model_save_path: str,
-                 model_file_name: str):
+    def __init__(self, model: torch.nn.Module, model_save_path=None,
+                 model_file_name=None):
         super(ImageRegressionPredictor, self).__init__(model, model_save_path,
                                                        model_file_name)
 
@@ -54,14 +56,14 @@ class ImageRegressionPredictor(Predictor):
             return None
 
         device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
-        self.model = self.model.to(device)
+        self.__model = self.__model.to(device)
         predictions = []
         targets = []
         with torch.no_grad():
             for X, y in tqdm(loader, total=len(loader), desc="{:12s}".format('Prediction')):
                 if use_gpu:
                     X = X.to(device)
-                y_pred = self.model(X).cpu()
+                y_pred = self.__model(X).cpu()
                 predictions.append(y_pred)
                 targets.append(y)
 
@@ -73,10 +75,12 @@ class ImageRegressionPredictor(Predictor):
 
 class ImageClassificationPredictor(Predictor):
 
-    def __init__(self, model: torch.nn.Module, model_save_path: str,
-                 model_file_name: str):
+    def __init__(self, model: torch.nn.Module, model_save_path=None,
+                 model_file_name=None, classes=None):
         super(ImageClassificationPredictor, self).__init__(model, model_save_path,
                                                            model_file_name)
+
+        self.classes = classes
 
     def predict_one(self, input: torch.Tensor, use_gpu=False):
         raise NotImplementedError()
