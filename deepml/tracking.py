ed6f2c48e3b8cc64adb319041b8850c217e0d7cf
Sagar Rathod <sagar.rathod@tomtom.com>
2023-01-08 19:22:39 +0530

    integrates with wandb

--- deepml/tracking.py
+++ deepml/tracking.py
@@ -3,7 +3,6 @@ from abc import ABC, abstractmethod
 from typing import Any, Optional
 
 import torch
-import wandb
 from torch.utils.tensorboard import SummaryWriter
 
 from deepml import utils
@@ -78,37 +77,47 @@ class MLFlowLogger(MLExperimentLogger):
 
     def __init__(self, experiment_name: str = "Default", tracking_uri: str = None):
         super().__init__()
-        MLFlowLogger.mlflow.set_experiment(experiment_name)
+        self.mlflow.set_experiment(experiment_name)
 
         if tracking_uri:
-            MLFlowLogger.mlflow.set_tracking_uri(tracking_uri)
+            self.mlflow.set_tracking_uri(tracking_uri)
 
     def log_params(self, **kwargs):
-        MLFlowLogger.mlflow.log_params(kwargs)
+        self.mlflow.log_params(kwargs)
 
     def log_metric(self, tag: str, value: Any, step: int):
-        MLFlowLogger.mlflow.log_metric(tag, value, step)
+        self.mlflow.log_metric(tag, value, step)
 
     def log_artifact(self, tag: str, value: Any, step: int, artifact_path: Optional[str] = None):
 
         if isinstance(value, dict):
-            MLFlowLogger.mlflow.pytorch.log_model(value, tag)
+            self.mlflow.pytorch.log_model(value, tag)
 
 
 class WandbLogger(MLExperimentLogger):
 
-    def __init__(self, **kwargs: dict):
+    try:
         import wandb
+    except ImportError as e:
+        print(e)
+
+    def __init__(self, **kwargs: dict):
         super().__init__()
         if kwargs:
-            wandb.init(*kwargs)
+            self.wandb.init(*kwargs)
 
     def log_params(self, **kwargs):
-        pass
+        self.wandb.config.update(kwargs)
 
     def log_metric(self, tag: str, value: Any, step: int):
-        wandb.log({tag: value})
+        self.wandb.log({tag: value})
 
     def log_artifact(self, tag: str, value: Any, step: int, artifact_path: Optional[str] = None):
         if artifact_path is not None and os.path.exists(artifact_path):
-            wandb.log_artifact(artifact_path, name=tag)
+            self.wandb.log_artifact(artifact_path, name=tag)
+
+        if isinstance(value, torch.Tensor) and value.ndim == 4:
+            # TODO: log image
+            pass
+
+
